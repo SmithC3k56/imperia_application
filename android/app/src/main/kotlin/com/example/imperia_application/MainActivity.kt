6a63200dfce0cf3073a308800f4813da640741d1
Mister Smith <son10c3k56@gmail.com>
2022-06-26 09:27:28 +0700

    the first
    the first commit

---
+++ android/app/src/main/kotlin/com/example/imperia_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imperia_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/UI/views/LoginPage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
